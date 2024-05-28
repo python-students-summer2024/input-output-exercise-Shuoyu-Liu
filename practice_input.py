6191bb85cceffbf49bda7919fa99ea9eb4e4471d
Shuoyu-Liu <sl10714@nyu.edu>
2024-05-28 18:42:08 +0800

    finished input and output

--- practice_input.py
+++ practice_input.py
@@ -10,7 +10,8 @@ def get_favorite_vegetable():
     and then prints out, "Interesting! I also love X!",
     where X is replaced with the user's favorite vegetable.
     """
-    # write your code here.
+    favorite_vegetable = input("tomato")
+    print(f"Interesting! I also love tomato!")
 
 
 def get_favorite_number():
@@ -19,7 +20,8 @@ def get_favorite_number():
     and then prints out, "Interesting! I also love X!",
     where X is replaced with the user's favorite number.
     """
-    # write your code here.
+    favorite_number = input("11")
+    print(f"Interesting! I also love 11!")
 
 
 def get_name_and_zodiac_sign():
@@ -29,7 +31,9 @@ def get_name_and_zodiac_sign():
     Then print out, "Interesting! My name is also X, and I'm also a Y!",
     where X and Y are replaced by the user's name and zodiac sign, respectively.
     """
-    # write your code here.
+    name = input("Jim")
+    zodiac_sign = input("Taurus")
+    print(f"Interesting! My name is also Jim, and I'm also a Taurus!")
 
 
 def get_name_and_age():
@@ -39,4 +43,6 @@ def get_name_and_age():
     Then print out, "Interesting! My name is also X, and I'm also Y years old!",
     where X and Y are replaced by the user's name and age, respectively.
     """
-    # write your code here.
+    name = input("Jim")
+    age = input("19")
+    print(f"Interesting! My name is also Jim, and I'm also 19 years old!")
